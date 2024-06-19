177a6fef057db8e86864624d9c7e940ba721e605
Alya fatimah <alyafatimah1122@gmail.com>
2024-06-19 21:22:13 +0700

    main

--- js/script.js
+++ js/script.js
@@ -26,38 +26,68 @@ function back(){
     window.location.href = 'index.html';
 }
 
-function convertCelcius(){
-    let celciusValue = document.getElementById("input-celcius").value;
-    let celciusNum = Number(celciusValue);
-    fahrenheit = (celciusNum * (9/5)) + 32;
-    fahrenheitString = fahrenheit.toString();
+function convertCelcius(celciusValue){
+    let fahrenheit = (celciusValue * (9/5)) + 32;
+    let fahrenheitString = fahrenheit.toString();
     document.getElementById("output-fahrenheit").value = fahrenheitString;
     document.getElementById("calculation-fahrenheit").value = `${celciusValue}℃ * (9/5) + 32 = ${fahrenheitString}℉`
 }
 
-function convertFahrenheit(){
-    let fahrenheitValue = document.getElementById("input-fahrenheit").value;
-    let fahrenheitNum = Number(fahrenheitValue);
-    celcius = (fahrenheitNum - 32) * (5/9);
-    celciusString = celcius.toString();
+function convertFahrenheit(fahrenheitValue){
+    let celcius = (fahrenheitValue - 32) * (5/9);
+    let celciusString = celcius.toString();
     document.getElementById("output-celcius").value = celciusString;
     document.getElementById("calculation-celcius").value = `(${fahrenheitValue}℉ - 32) * (5/9) = ${celciusString}℃`
 }
 
 function resetCelcius(){
-    celciusNum = 0;
-    fahrenheit = 0;
-    
     document.getElementById("input-celcius").value = '';
     document.getElementById("output-fahrenheit").value = '';
     document.getElementById("calculation-fahrenheit").value = '';
 }
 
 function resetFahrenheit(){
-    celcius = 0;
-    fahrenheitNum = 0;
-
     document.getElementById("input-fahrenheit").value = '';
     document.getElementById("output-celcius").value = '';
     document.getElementById("calculation-celcius").value = '';
+}
+
+function validateFormCelcius() {
+    const celciusInput = document.getElementById('input-celcius');
+    const errorMessage = document.getElementById('error-message');
+    const celciusValue = parseFloat(celciusInput.value);
+
+    errorMessage.textContent = '';
+
+    if (isNaN(celciusValue)) {
+        errorMessage.textContent = 'Mohon masukkan angka yang valid';
+        return false;
+    }
+
+    if (celciusValue < -273.15) {
+        errorMessage.textContent = 'Suhu tidak boleh kurang dari titik 0 mutlak(-273.15°C).';
+        return false;
+    }
+
+    return convertCelcius(celciusValue);
+}
+
+function validateFormFahrenheit() {
+    const fahrenheitInput = document.getElementById('input-fahrenheit');
+    const errorMessage = document.getElementById('error-message');
+    const fahrenheitValue = parseFloat(fahrenheitInput.value);
+
+    errorMessage.textContent = '';
+
+    if (isNaN(fahrenheitValue)) {
+        errorMessage.textContent = 'Mohon masukkan angka yang valid';
+        return false;
+    }
+
+    if (fahrenheitValue < -459.67) {
+        errorMessage.textContent = 'Suhu tidak boleh kurang dari titik 0 mutlak(-459.67°F).';
+        return false;
+    }
+
+    return convertFahrenheit(fahrenheitValue);
 }
